2cf0844a23961d7ac127ae9811f2334b2e9e5c83
Elise Erickson <elisece717@gmail.com>
2023-12-01 12:57:24 -0500

    satisfies tests

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+let addToDOM = (string) => {
+    //let container = document.createElement('div');
+    let object = document.createElement('h1');
+    object.textContent = string;
+    document.body.append(object);
+}
+
+let submitData = (name, email) => {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name, email}),
+    };
+
+    let fetchRequest = fetch('http://localhost:3000/users', configurationObject)
+    .then(res => res.json())
+    .then(object => addToDOM(object.id))
+    .catch(e => addToDOM(e.message))
+    return(fetchRequest);
+}
+
+let init = () => {
+    let button = document.querySelector("button");
+    button.addEventListener("click", () => submitData());
+}
+
+
+document.addEventListener("DOMContentLoaded", () => init());
